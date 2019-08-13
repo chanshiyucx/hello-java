a722a73befed4681dc5c2c611fe71ae666dff6c8
chanshiyu <chanshiyu@tg10010.com>
2019-08-13 15:04:49 +0700

    socket

--- sell-admin/src/config.js
+++ sell-admin/src/config.js
@@ -6,5 +6,6 @@ if (!isDev) {
 }
 
 export default {
-  baseURL: 'http://127.0.0.1/'
+  baseURL: 'http://127.0.0.1/',
+  wsURL: 'ws://127.0.0.1:8080/webSocket'
 }
--- sell-admin/src/views/dashboard/index.vue
+++ sell-admin/src/views/dashboard/index.vue
@@ -5,11 +5,39 @@
 </template>
 
 <script>
+import config from '@/config'
+
 export default {
   name: 'Dashboard',
   data() {
     return {
-      emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
+      emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3',
+      wsUrl: '',
+      socket: null
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.socket = new WebSocket(config.wsURL)
+      this.socket.onopen = () => this.onopen()
+      this.socket.onclose = () => this.onclose()
+      this.socket.onerror = () => this.onerror()
+      this.socket.onmessage = event => this.onmessage(event)
+    },
+    onopen() {
+      console.log('连接成功')
+    },
+    onclose() {
+      console.log('连接关闭')
+    },
+    onerror() {
+      console.log('连接错误')
+    },
+    onmessage(event) {
+      console.log('接收消息', event)
     }
   }
 }

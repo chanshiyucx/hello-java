815431886ff9e0843ceef6f27cfd05ca7e33b8b9
chanshiyu <chanshiyu@tg10010.com>
2019-09-10 21:04:03 +0700

    update

--- shiyu-h5/src/views/Me/index.vue
+++ shiyu-h5/src/views/Me/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="me">
-    <van-nav-bar title="我的" class="header" />
+    <van-nav-bar title="我的" class="header" @click-right="onClickRight">
+      <van-icon name="scan" slot="right" />
+    </van-nav-bar>
     <van-cell-group>
       <van-cell is-link title="头像" class="avatar-cell" to="/avatar">
         <img class="avatar" slot="default" :src="userInfo.avatar" alt="头像" />
@@ -11,16 +13,26 @@
         <van-icon slot="default" name="qr" class="icon" size="20" />
       </van-cell>
     </van-cell-group>
-
     <div class="logout" @click="handleLogout">退出登录</div>
+    <!-- <QrcodeStream @decode="onDecode" @init="onInit" /> -->
+    <qrcode-stream @decode="onDecode" @init="onInit" />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 export default {
   name: 'me',
+  components: { QrcodeStream },
+  data() {
+    return {
+      visible: {
+        qrcode: false
+      }
+    }
+  },
   computed: {
     ...mapGetters(['userInfo'])
   },
@@ -29,8 +41,31 @@ export default {
       logout: 'logout'
     }),
     handleLogout() {
-      this.logout()
-      this.$router.push('/login')
+      this.$dialog
+        .confirm({
+          title: '退出登录',
+          message: '确定退出该账号？'
+        })
+        .then(() => {
+          this.logout()
+          this.$router.push('/login')
+        })
+        .catch(() => {
+          console.log('取消')
+        })
+    },
+    async onInit(promise) {
+      try {
+        await promise
+      } catch (error) {
+        this.$toast.fail('扫码失败')
+      }
+    },
+    onClickRight() {
+      this.visible.qrcode = true
+    },
+    onDecode(result) {
+      console.log('result-->', result)
     }
   }
 }
--- shiyu-h5/src/views/Qrcode/index.vue
+++ shiyu-h5/src/views/Qrcode/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="qrcode">
     <van-nav-bar title="我の二维码" class="header" left-arrow @click-left="onClickLeft" />
-    <!-- <qrcode-stream @decode="onDecode" @init="onInit" /> -->
     <div class="main">
       <div class="header">
         <img :src="userInfo.avatar" alt="头像" />
@@ -18,42 +17,17 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { QrcodeStream } from 'vue-qrcode-reader'
 import VueQrcode from '@chenfengyuan/vue-qrcode'
 
 export default {
   name: 'qrcode',
-  components: { QrcodeStream, VueQrcode },
+  components: { VueQrcode },
   computed: {
     ...mapGetters(['userInfo'])
   },
   methods: {
     onClickLeft() {
       this.$router.go(-1)
-    },
-    async onInit(promise) {
-      try {
-        await promise
-      } catch (error) {
-        let errorMsg
-        if (error.name === 'NotAllowedError') {
-          errorMsg = 'ERROR: you need to grant camera access permisson'
-        } else if (error.name === 'NotFoundError') {
-          errorMsg = 'ERROR: no camera on this device'
-        } else if (error.name === 'NotSupportedError') {
-          errorMsg = 'ERROR: secure context required (HTTPS, localhost)'
-        } else if (error.name === 'NotReadableError') {
-          errorMsg = 'ERROR: is the camera already in use?'
-        } else if (error.name === 'OverconstrainedError') {
-          errorMsg = 'ERROR: installed cameras are not suitable'
-        } else if (error.name === 'StreamApiNotSupportedError') {
-          errorMsg = 'ERROR: Stream API is not supported in this browser'
-        }
-        this.$toast.fail(errorMsg)
-      }
-    },
-    onDecode(result) {
-      this.result = result
     }
   }
 }
